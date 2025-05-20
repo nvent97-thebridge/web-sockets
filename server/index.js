f84b53ea195fb2f632b579c38dc92571a1c60a53
xavi <antunez.xavi.grao@gmail.com>
2025-05-20 12:58:02 +0200

    Basic chat functionality

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 import { Server } from "socket.io";
 
-const io = new Server(3000);
+const io = new Server(3000, { cors: {
+  origin: "*"
+} });
 
 io.on("connection", (socket) => {
   console.log("CONEXION RECIBIDA", socket.id);
 
   // Escuchar del cliente:
   socket.on("mensaje", (arg) => {
-    console.log(arg);
     io.emit("respuesta", `${socket.id} escribio: ${arg}`);
   });
 });
