346b7e2b027ae0edd2babcad5a6fe94374844de2
xavi <antunez.xavi.grao@gmail.com>
2025-05-20 13:00:47 +0200

    Client code

--- client/script.js
+++ client/script.js
@@ -1,10 +1,20 @@
-let socket = io();
+let socket = io("http://localhost:3000");
 
-// Que cuando presione el boton muestre input en la consola
+const btnSend = document.getElementById("btnSend");
+const inputMessage = document.getElementById("inputMessage");
+const listMessages = document.getElementById("listMessages");
 
+const sendMessage = () => {
+  const message = inputMessage.value;
+  socket.emit("mensaje", message);
+  inputMessage.value = ""
+};
 
+const listenMessages = (message) => {
+  listMessages.innerHTML += `
+    <li>${message}</li>
+ `;
+};
 
-
-
-// Conectarme al server
-// Emitir mensaje con lo que el usuario escribio en el input
+socket.on("respuesta", listenMessages);
+btnSend.addEventListener("click", sendMessage);
