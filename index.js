6179f3ef1e564d24a105004d765e4dee629fcf55
xavi <antunez.xavi.grao@gmail.com>
2025-05-20 10:53:36 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import { Server } from "socket.io";
+
+const io = new Server(3000);
+
+io.on("connection", (socket) => {
+  console.log("CONEXION RECIBIDA", socket.id);
+
+  // Escuchar del cliente:
+  socket.on("mensaje", (arg) => {
+    console.log(arg);
+    io.emit("respuesta", `${socket.id} escribio: ${arg}`);
+  });
+});
